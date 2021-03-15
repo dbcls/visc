c2097bc72e3cf9cb13f28860e31defc207d40a4a
Takatomo Fujisawa <fujisawa.takatomo@gmail.com>
2021-03-15 17:31:01 +0900

    Changed to an external file of ERB template file in convert-rdf.rb

--- convert-rdf.rb
+++ convert-rdf.rb
@@ -1,6 +1,8 @@
 require 'bio-vcf'
 require 'bio-vcf/vcffile'
 require 'erb'
+require "pathname"
+
 
 module BioVcf
   class VcfRecord
@@ -114,9 +116,21 @@ module VISC
 '
       end
 
+      def template2ttl(type)
+        #root = Pathname.getwd #=> #<Pathname:/home/zzak/projects/ruby>
+        template = File.read(File.expand_path("../template/#{type.downcase}.erb", __FILE__))
+        #template = File.read("#{root}/template/#{type.downcase}.erb")
+        #ttl = ERB.new(template).run
+        ttl = ERB.new(template).result( binding )
+        puts ttl
+        
+      end
+
       def to_ttl(obj)
         @obj = obj
-        puts to_ttl_prefix
+        puts to_ttl_prefix unless @prefix
+        @prefix = true
+
         puts "### " + @obj.fields.inspect
 
         obj.alt.each do |alt_mono|
@@ -128,184 +142,25 @@ module VISC
           @alt_mono_vcf = alt_mono
           #@alt_mono = alt_mono
           case obj.info['VC']
-          when 'DEL'
-            ERB.new(del).run
-          when 'SNV'
-            ERB.new(snv).run
-          when 'INS'
-            ERB.new(ins).run
-          when 'INDEL'
-            ERB.new(delins).run 
-          when 'MNV'
-            ERB.new(mnv).run                                
+          when 'DEL','SNV','INS','INDEL','MNV'
+            template2ttl(obj.info['VC'])
           else
+            case obj.info['SVTYPE']
+              #CNV, BND
+                pp obj
+              else
+            begin 
+              raise # エラーを発生させます。
+            rescue => e
+              STDERR.puts "error: unknown type on VCF" 
+              STDERR.pp obj
+              exit
+            end
+          end
           #p obj
           end
         end
       end
-      def mnv
-"
-
-[ 
-  rdf:type gvo:MNV ;
-  faldo:location [
-    a faldo:Region ;
-    faldo:begin #{@obj.pos} ;
-    faldo:end #{@obj.pos + @obj.ref.length - 1} ;
-    faldo:reference <http://identifiers.org/refseq:NC_000001.10>
-  ];
-
-  rdfs:seeAlso \"#{@obj.id}\" ;
-  gvo:ref \"#{@nref}\" ;
-  gvo:alt  \"#{@nalt}\" ;
-  gvo:qual \"#{@obj.qual}\" ;
-  gvo:filter \"#{@obj.filter}\" ;
-  gvo:info []
-  ];
-
-] .
-"
-  # http://purl.obolibrary.org/obo/SO_0002007
-  # sequence_feature > sequence_comparison> sequence_alteration > substitution > MNV
-  # rdf:type gvo:MNV ;                               # variant type
-  # rdfs:subClassOf obo:SO_0002007 ;                 # obo:SO_0002007 (MNV) 
-  # rdfs:subClassOf obo:SO_0000001 ;               # obo:SO_0000001 (region)
-
-      end
-      def delins
-"
-
-[ 
-
-  rdf:type gvo:INDEL ;
-  faldo:location [
-    a faldo:Region ;
-    faldo:begin [
-      a faldo:InBetweenPosition ;
-      faldo:after #{@npos} ;
-      faldo:before #{@npos + 1} ;
-      faldo:reference <#{reference_uri(@obj.chrom)}>
-    ] ;
-    faldo:end [
-      a faldo:InBetweenPosition ;
-      faldo:after #{@npos + @nref.length} ;
-      faldo:before #{@npos + 1 + @nref.length } ;
-      faldo:reference <#{reference_uri(@obj.chrom)}>
-    ] 
-  ];
-  rdfs:seeAlso \"#{@obj.id}\" ;
-  gvo:ref \"#{@nref}\" ;
-  gvo:alt \"#{@alt}\" ;
-  gvo:ref_vcf \"#{@obj.ref}\" ;
-  gvo:alt_vcf  \"#{@alt_mono_vcf}\" ;
-  gvo:qual \"#{@obj.qual}\" ;
-  gvo:filter \"#{@obj.filter}\" ;
-  gvo:info []
-].
-"
-# rdfs:subClassOf obo:SO_0000366 ;               # obo:SO_0000366 (insertion_site)
-# rdfs:subClassOf obo:SO_0000159 ;               # obo:SO_0000687 (deletion_junction)
-# http://purl.obolibrary.org/obo/SO_1000032
-# sequence_feature > sequence_comparison> sequence_alteration > delins
-# rdfs:subClassOf obo:SO_1000032 ;                 # obo:SO_1000032 (delins)  
-      end
-      def ins
-"
-
-[ 
-  rdf:type gvo:Insertion ;
-  faldo:location [
-    a faldo:InBetweenPosition ;
-    faldo:after #{@npos} ;      # info.pos
-    faldo:before #{@npos + 1} ; # info.pos + 1
-    faldo:reference <#{reference_uri(@obj.chrom)}> 
-  ];
-
-  rdfs:seeAlso \"#{@obj.id}\" ;
-  gvo:ref \"#{@nref}\" ;
-  gvo:alt \"#{@nalt}\" ;
-  gvo:ref_vcf \"#{@obj.ref}\" ;
-  gvo:alt_vcf  \"#{@alt_mono_vcf}\" ;
-  gvo:qual \"#{@obj.qual}\" ;
-  gvo:filter \"#{@obj.filter}\" ;
-  gvo:info []
-
-].
-"
-  # http://purl.obolibrary.org/obo/SO_0000667
-  # sequence_feature > sequence_comparison> sequence_alteration > insertion
-  # rdfs:subClassOf obo:SO_0000366 ;               # obo:SO_0000366 (insertion_site)
-  # rdfs:subClassOf obo:SO_0000667 ;               # obo:SO_0000667 (insertion)
-      end
-      def del
-"
-
-[ 
-  rdf:type gvo:Deletion ; # vcf.info['VC']  = DEL
-  faldo:location [
-    a faldo:Region ;
-    faldo:begin [
-      a faldo:InBetweenPosition ;
-      faldo:after #{@npos} ;
-      faldo:before #{@npos + 1} ;
-      faldo:reference <#{reference_uri(@obj.chrom)}>
-    ] ;
-    faldo:end [
-      a faldo:InBetweenPosition ;
-      faldo:after #{@npos + @nref.length} ;
-      faldo:before #{@npos + 1 + @nref.length } ;
-      faldo:reference <#{reference_uri(@obj.chrom)}>
-    ] 
-  ];
-
-  rdfs:seeAlso \"#{@obj.id}\" ;
-  gvo:ref \"#{@nref}\" ;
-  gvo:alt \"#{@nalt}\" ;
-  gvo:ref_vcf \"#{@obj.ref}\" ;
-  gvo:alt_vcf  \"#{@obj.alt.first}\" ;
-  gvo:qual \"#{@obj.qual}\" ;
-  gvo:filter \"#{@obj.filter}\" ;
-  gvo:info []
-].
-"
-  # http://purl.obolibrary.org/obo/SO_0000159
-  # sequence_feature > sequence_comparison> sequence_alteration > deletion
-  # obo:SO_0000687 (deletion_junction)            
-      end
-      def snv
-"
-
-[
-  # dbSNP: vcf.info['VC'], ...
-  rdf:type gvo:SNV ;
-  faldo:location [
-    a faldo:ExactPosition ;
-    faldo:position #{@npos} ;
-    faldo:reference <#{reference_uri(@obj.chrom)}> ;
-    # TODO
-    #faldo:reference hco:1:GRCh37 ;
-  ];
-
-  # vcf.id 
-  rdfs:seeAlso \"#{@obj.id}\" ;
-  # vcf.ref.normalized
-  gvo:ref \"#{@nref}\" ;
-  # vcf.alt.normalized
-  gvo:alt \"#{@nalt}\" ;
-  # vcf.qual
-  gvo:qual \"#{@obj.qual}\" ;
-  # vcf.filter
-  gvo:filter \"#{@obj.filter}\" ;
-  # TODO
-  gvo:info []
-
-].
-"
-  # sequence_feature > sequence_comparison> sequence_alteration > substitution > SNV
-  # http://purl.obolibrary.org/obo/SO_0001483
-  # obo:SO_0001236 (base)
-  # TODO gvo:info [ rdf:label \"VC\"; sio:SIO_000300 \"DEL\"]
-        end
     end
   end
 end
